b05e027dfe5a3d271250061141e822ca9ba9f92e
Eduin Garcia Cordero <eduinlight@gmail.com>
2024-07-15 21:49:46 +0200

    adding acl

---
+++ wireguard/common.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SUBNET=10.8.0.0/24
+WG_INTERFACE=wg0
+WG_PORT=eth0
+OUT_INTERFACE=eth0
--- wireguard/postdown.sh
+++ wireguard/postdown.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
-iptables -D INPUT -p udp -m udp --dport 51820 -j ACCEPT
-iptables -D FORWARD -i wg0 -j ACCEPT
-iptables -D FORWARD -o wg0 -j ACCEPT
+source /etc/wireguard/ips.sh
+source /etc/wireguard/common.sh
+
+iptables -t nat -D POSTROUTING -s $SUBNET -o $OUT_INTERFACE -j MASQUERADE
+iptables -D INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
+
+if [ $MODE == "ALLOW_DENY" ]; then
+  iptables -D FORWARD -i $WG_INTERFACE -j ACCEPT
+  iptables -D FORWARD -o $WG_INTERFACE -j ACCEPT
+elif [ $MODE == "DENY_ALLOW" ]; then
+  iptables -D FORWARD -i $WG_INTERFACE -j DROP
+  iptables -D FORWARD -o $WG_INTERFACE -j DROP
+else
+  echo "NOT ALLOWED MODE ${MODE}"
+  exit 1
+fi
+
+for IP in ${IPS[@]}; do
+  if [ $MODE == "ALLOW_DENY" ]; then
+    iptables -D FORWARD -i $WG_INTERFACE -s $IP -j DROP
+    iptables -D FORWARD -o $WG_INTERFACE -d $IP -j DROP
+  else
+    iptables -D FORWARD -i $WG_INTERFACE -s $IP -j ACCEPT
+    iptables -D FORWARD -o $WG_INTERFACE -d $IP -j ACCEPT
+    echo $IP
+  fi
+done
