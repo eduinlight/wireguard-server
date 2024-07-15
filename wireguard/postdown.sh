9a3264c6bd1750540e2ebece3a549d68e31e7ff2
Eduin Garcia Cordero <eduinlight@gmail.com>
2024-07-16 00:21:26 +0200

    should work

--- wireguard/postdown.sh
+++ wireguard/postdown.sh
@@ -8,21 +8,21 @@ iptables -D INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
 iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
 iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
 
-# if [ $MODE == "DENY_ALLOW" ]; then
-#   for IP in ${IPS[@]}; do
-#     iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j DROP
-#     iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j DROP
-#   done
-#   iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
-#   iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
-# elif [ $MODE == "ALLOW_DENY" ]; then
-#   for IP in ${IPS[@]}; do
-#     iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j ACCEPT
-#     iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j ACCEPT
-#   done
-#   iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j DROP
-#   iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j DROP
-# else
-#   echo "NOT ALLOWED MODE ${MODE}"
-#   exit 1
-# fi
+if [ $MODE == "DENY_ALLOW" ]; then
+  for IP in ${IPS[@]}; do
+    iptables -D INPUT -i $OUT_INTERFACE -s $IP -j DROP
+    iptables -D OUTPUT -o $OUT_INTERFACE -d $IP -j DROP
+  done
+  iptables -D INPUT -i $OUT_INTERFACE -j ACCEPT
+  iptables -D OUTPUT -o $OUT_INTERFACE -j ACCEPT
+elif [ $MODE == "ALLOW_DENY" ]; then
+  for IP in ${IPS[@]}; do
+    iptables -D INPUT -i $OUT_INTERFACE -s $IP -j ACCEPT
+    iptables -D OUTPUT -o $OUT_INTERFACE -d $IP -j ACCEPT
+  done
+  iptables -D INPUT -i $OUT_INTERFACE -j DROP
+  iptables -D OUTPUT -o $OUT_INTERFACE -j DROP
+else
+  echo "NOT ALLOWED MODE ${MODE}"
+  exit 1
+fi
--- wireguard/postup.sh
+++ wireguard/postup.sh
@@ -8,19 +8,21 @@ iptables -A INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
 iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
 iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
 
-# if [ $MODE == "DENY_ALLOW" ]; then
-#   for IP in ${IPS[@]}; do
-#     iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j DROP
-#     iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j DROP
-#   done
-# elif [ $MODE == "ALLOW_DENY" ]; then
-#   for IP in ${IPS[@]}; do
-#     iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j ACCEPT
-#     iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j ACCEPT
-#   done
-#   iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j DROP
-#   iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j DROP
-# else
-#   echo "NOT ALLOWED MODE ${MODE}"
-#   exit 1
-# fi
+if [ $MODE == "DENY_ALLOW" ]; then
+  for IP in ${IPS[@]}; do
+    iptables -A INPUT -i $OUT_INTERFACE -s $IP -j DROP
+    iptables -A OUTPUT -o $OUT_INTERFACE -d $IP -j DROP
+  done
+  iptables -A INPUT -i $OUT_INTERFACE -j ACCEPT
+  iptables -A OUTPUT -o $OUT_INTERFACE -j ACCEPT
+elif [ $MODE == "ALLOW_DENY" ]; then
+  for IP in ${IPS[@]}; do
+    iptables -A INPUT -i $OUT_INTERFACE -s $IP -j ACCEPT
+    iptables -A OUTPUT -o $OUT_INTERFACE -d $IP -j ACCEPT
+  done
+  iptables -A INPUT -i $OUT_INTERFACE -j DROP
+  iptables -A OUTPUT -o $OUT_INTERFACE -j DROP
+else
+  echo "NOT ALLOWED MODE ${MODE}"
+  exit 1
+fi
