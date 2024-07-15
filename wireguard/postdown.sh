1e2634d070f8c119933aa3c6c90a5f161c8271db
Eduin Garcia Cordero <eduinlight@gmail.com>
2024-07-16 00:11:56 +0200

    should work

--- wireguard/postdown.sh
+++ wireguard/postdown.sh
@@ -5,22 +5,24 @@ source /etc/wireguard/common.sh
 
 iptables -t nat -D POSTROUTING -s $SUBNET -o $OUT_INTERFACE -j MASQUERADE
 iptables -D INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
+iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
+iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
 
-if [ $MODE == "DENY_ALLOW" ]; then
-  for IP in ${IPS[@]}; do
-    iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j DROP
-    iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j DROP
-  done
-  iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
-  iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
-elif [ $MODE == "ALLOW_DENY" ]; then
-  for IP in ${IPS[@]}; do
-    iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j ACCEPT
-    iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j ACCEPT
-  done
-  iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j DROP
-  iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j DROP
-else
-  echo "NOT ALLOWED MODE ${MODE}"
-  exit 1
-fi
+# if [ $MODE == "DENY_ALLOW" ]; then
+#   for IP in ${IPS[@]}; do
+#     iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j DROP
+#     iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j DROP
+#   done
+#   iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
+#   iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
+# elif [ $MODE == "ALLOW_DENY" ]; then
+#   for IP in ${IPS[@]}; do
+#     iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j ACCEPT
+#     iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j ACCEPT
+#   done
+#   iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j DROP
+#   iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j DROP
+# else
+#   echo "NOT ALLOWED MODE ${MODE}"
+#   exit 1
+# fi
--- wireguard/postup.sh
+++ wireguard/postup.sh
@@ -5,22 +5,22 @@ source /etc/wireguard/common.sh
 
 iptables -t nat -A POSTROUTING -s $SUBNET -o $OUT_INTERFACE -j MASQUERADE
 iptables -A INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
+iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
+iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
 
-if [ $MODE == "DENY_ALLOW" ]; then
-  for IP in ${IPS[@]}; do
-    iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j DROP
-    iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j DROP
-  done
-  iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
-  iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
-elif [ $MODE == "ALLOW_DENY" ]; then
-  for IP in ${IPS[@]}; do
-    iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j ACCEPT
-    iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j ACCEPT
-  done
-  iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j DROP
-  iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j DROP
-else
-  echo "NOT ALLOWED MODE ${MODE}"
-  exit 1
-fi
+# if [ $MODE == "DENY_ALLOW" ]; then
+#   for IP in ${IPS[@]}; do
+#     iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j DROP
+#     iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j DROP
+#   done
+# elif [ $MODE == "ALLOW_DENY" ]; then
+#   for IP in ${IPS[@]}; do
+#     iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -s $IP -j ACCEPT
+#     iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -d $IP -j ACCEPT
+#   done
+#   iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j DROP
+#   iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j DROP
+# else
+#   echo "NOT ALLOWED MODE ${MODE}"
+#   exit 1
+# fi
