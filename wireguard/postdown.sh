20234cbbc6a55a01fb2e5fc0e4d75dadaefc7fbd
Eduin Garcia Cordero <eduinlight@gmail.com>
2024-07-16 20:00:40 +0200

    something happend

--- wireguard/postdown.sh
+++ wireguard/postdown.sh
@@ -8,24 +8,24 @@ iptables -D INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
 iptables -D FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
 iptables -D FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
 # allow dns
-iptables -D FORWARD -d $WG_DEFAULT_DNS -j ACCEPT
-iptables -D FORWARD -s $WG_DEFAULT_DNS -j ACCEPT
-
-if [ $ACL_MODE == "DENY_SOME_ALLOW_ALL" ]; then
-  for IP in ${IPS[@]}; do
-    iptables -D FORWARD -i $OUT_INTERFACE -s $IP -j DROP
-    iptables -D FORWARD -o $OUT_INTERFACE -d $IP -j DROP
-  done
-  iptables -D FORWARD -i $OUT_INTERFACE -j ACCEPT
-  iptables -D FORWARD -o $OUT_INTERFACE -j ACCEPT
-elif [ $ACL_MODE == "ALLOW_SOME_DENY_ALL" ]; then
-  for IP in ${IPS[@]}; do
-    iptables -D FORWARD -i $OUT_INTERFACE -s $IP -j ACCEPT
-    iptables -D FORWARD -o $OUT_INTERFACE -d $IP -j ACCEPT
-  done
-  iptables -D FORWARD -i $OUT_INTERFACE -j DROP
-  iptables -D FORWARD -o $OUT_INTERFACE -j DROP
-else
-  echo "NOT ALLOWED ACM_MODE ${ACL_MODE}"
-  exit 1
-fi
+# iptables -D FORWARD -d $WG_DEFAULT_DNS -j ACCEPT
+# iptables -D FORWARD -s $WG_DEFAULT_DNS -j ACCEPT
+#
+# if [ $ACL_MODE == "DENY_SOME_ALLOW_ALL" ]; then
+#   for IP in ${IPS[@]}; do
+#     iptables -D FORWARD -i $OUT_INTERFACE -s $IP -j DROP
+#     iptables -D FORWARD -o $OUT_INTERFACE -d $IP -j DROP
+#   done
+#   iptables -D FORWARD -i $OUT_INTERFACE -j ACCEPT
+#   iptables -D FORWARD -o $OUT_INTERFACE -j ACCEPT
+# elif [ $ACL_MODE == "ALLOW_SOME_DENY_ALL" ]; then
+#   for IP in ${IPS[@]}; do
+#     iptables -D FORWARD -i $OUT_INTERFACE -s $IP -j ACCEPT
+#     iptables -D FORWARD -o $OUT_INTERFACE -d $IP -j ACCEPT
+#   done
+#   iptables -D FORWARD -i $OUT_INTERFACE -j DROP
+#   iptables -D FORWARD -o $OUT_INTERFACE -j DROP
+# else
+#   echo "NOT ALLOWED ACM_MODE ${ACL_MODE}"
+#   exit 1
+# fi
