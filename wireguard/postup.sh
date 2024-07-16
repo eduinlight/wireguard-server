672f9063fff19a8667c11fb62498c26ec0cb2f21
Eduin Garcia Cordero <eduinlight@gmail.com>
2024-07-16 19:59:13 +0200

    changi to forward

--- wireguard/postup.sh
+++ wireguard/postup.sh
@@ -8,23 +8,23 @@ iptables -A INPUT -p udp -m udp --dport $WG_PORT -j ACCEPT
 iptables -A FORWARD -i $WG_INTERFACE -o $OUT_INTERFACE -j ACCEPT
 iptables -A FORWARD -i $OUT_INTERFACE -o $WG_INTERFACE -j ACCEPT
 # allow dns
-iptables -A POSTROUTING -d $WG_DEFAULT_DNS -j ACCEPT
-iptables -A POSTROUTING -s $WG_DEFAULT_DNS -j ACCEPT
+iptables -A FORWARD -d $WG_DEFAULT_DNS -j ACCEPT
+iptables -A FORWARD -s $WG_DEFAULT_DNS -j ACCEPT
 
 if [ $ACL_MODE == "DENY_SOME_ALLOW_ALL" ]; then
   for IP in ${IPS[@]}; do
-    iptables -A POSTROUTING -i $OUT_INTERFACE -s $IP -j DROP
-    iptables -A POSTROUTING -o $OUT_INTERFACE -d $IP -j DROP
+    iptables -A FORWARD -i $OUT_INTERFACE -s $IP -j DROP
+    iptables -A FORWARD -o $OUT_INTERFACE -d $IP -j DROP
   done
-  iptables -A POSTROUTING -i $OUT_INTERFACE -j ACCEPT
-  iptables -A POSTROUTING -o $OUT_INTERFACE -j ACCEPT
+  iptables -A FORWARD -i $OUT_INTERFACE -j ACCEPT
+  iptables -A FORWARD -o $OUT_INTERFACE -j ACCEPT
 elif [ $ACL_MODE == "ALLOW_SOME_DENY_ALL" ]; then
   for IP in ${IPS[@]}; do
-    iptables -A POSTROUTING -i $OUT_INTERFACE -s $IP -j ACCEPT
-    iptables -A POSTROUTING -o $OUT_INTERFACE -d $IP -j ACCEPT
+    iptables -A FORWARD -i $OUT_INTERFACE -s $IP -j ACCEPT
+    iptables -A FORWARD -o $OUT_INTERFACE -d $IP -j ACCEPT
   done
-  iptables -A POSTROUTING -i $OUT_INTERFACE -j DROP
-  iptables -A POSTROUTING -o $OUT_INTERFACE -j DROP
+  iptables -A FORWARD -i $OUT_INTERFACE -j DROP
+  iptables -A FORWARD -o $OUT_INTERFACE -j DROP
 else
   echo "NOT ALLOWED ACL_MODE ${ACL_MODE}"
   exit 1
